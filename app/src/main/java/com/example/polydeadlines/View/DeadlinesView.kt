66b1d5e8b09485541a488d0e9acc727b36e9f43b
matvey <tommathilfiger@gmail.com>
2024-04-20 20:32:25 +0300

    Renamed MainView to DeadlinesView

--- app/src/main/java/com/example/polydeadlines/View/DeadlinesView.kt
+++ app/src/main/java/com/example/polydeadlines/View/DeadlinesView.kt
@@ -25,15 +25,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.DialogProperties
 
 
-//@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Auth(onDismis: ()->Unit) {
@@ -109,7 +105,7 @@ fun TopBar() {
         Text("Content",Modifier.padding(innerPadding))
     }
     if(openDialog.value)
-        Auth { openDialog.value = false };
+        Auth { openDialog.value = false }
 }
 
 /*
