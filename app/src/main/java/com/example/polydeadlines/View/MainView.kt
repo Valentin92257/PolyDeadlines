7bd81498e51aa50706194be0ec79aad1936f4ad3
matvey <tommathilfiger@gmail.com>
2024-04-20 19:44:39 +0300

    Top Bar,Log in action

--- app/src/main/java/com/example/polydeadlines/View/MainView.kt
+++ app/src/main/java/com/example/polydeadlines/View/MainView.kt
@@ -1,28 +1,118 @@
 package com.example.polydeadlines.View
 
-import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 
 
+//@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Auth(onDismis: ()->Unit) {
+    val mail = remember { mutableStateOf("") }
+    BasicAlertDialog(
+        onDismissRequest =onDismis,
+    ) {
+        Column(Modifier.background(Color.White, MaterialTheme.shapes.medium)) {
+            Text(
+                text = "Введите логин:",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                color = Color.Black,
+                fontSize = 15.sp,
+                textAlign = TextAlign.Center
+            )
+            TextField(
+                value = mail.value,
+                onValueChange = { newText -> mail.value = newText },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+            )
+            Row(
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Button(
+                    modifier = Modifier
+                        .weight(1.0F),
+                    onClick = onDismis) {
+                    Text("Отмена")
+                }
+                Spacer(modifier = Modifier.width(5.dp))
+                Button(
+                    modifier = Modifier
+                        .weight(1.0F),
+                    onClick = onDismis) {
+                    Text("Ок")
+                }
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    val openDialog = remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Gray,
+                    titleContentColor = Color.Cyan,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                },
+                actions = {
+                    IconButton(onClick = {openDialog.value = true}){
+                        Icon(
+                            imageVector = Icons.Filled.AccountCircle,
+                            contentDescription = "Log in"
+                        )
+                    }
+                }
+            )
+        },
+    ) {
+        innerPadding->
+        Text("Content",Modifier.padding(innerPadding))
+    }
+    if(openDialog.value)
+        Auth { openDialog.value = false };
+}
+
+/*
 @Composable
 fun DeadLineCard(data: DeadLineData) {
     val checkedState = remember { mutableStateOf(data.isActive) }
@@ -59,4 +149,4 @@ fun DeadLineCard(data: DeadLineData) {
 
         }
     }
-}
+}*/
